14675df859cce3afd5689ba50f1a2a60597a0714
DSPT-YULIAN <yulian@DESKTOP-UDQK6GV>
2019-02-10 11:02:06 -0500

    radio ok

--- funciones/src/funciones/areacirculo.java
+++ funciones/src/funciones/areacirculo.java
@@ -1,11 +1,28 @@
 package funciones;
 
+
+import javax.swing.JOptionPane;
+
 public class areacirculo {
+	
+	
+	public static double globalradio = 0;
+	public static double globalvolumen = 0;
 
 	public static void main(String[] args) {
 
-
 		
+		globalradio = Double.parseDouble(JOptionPane.showInputDialog("ingresa el radio"));
+		
+
+		globalradio = Math.PI * Math.pow(globalradio, 2);
+		globalvolumen = (4.0/3.0) * Math.PI  *(Math.pow(globalradio, 3));
+		
+
+		System.out.println("El area del circulo es :"+ globalradio);   
+
+		System.out.println("El volumen de la esfera es :"+ globalvolumen);   
+
 
 	}
 
