681987301eb6a1cc8f6f88aed0ea47bac140547a
DSPT-YULIAN <yulian@DESKTOP-UDQK6GV>
2019-02-10 20:16:05 -0500

    Final

--- funciones/src/funciones/areacirculo.java
+++ funciones/src/funciones/areacirculo.java
@@ -1,29 +1,41 @@
 package funciones;
 
-
 import javax.swing.JOptionPane;
 
 public class areacirculo {
 	
+	public  static  double globalradio =  0 ;
+	public  static  double globalvolumen =  0 ;
 	
-	public static double globalradio = 0;
-	public static double globalvolumen = 0;
-
+	
+	
 	public static void main(String[] args) {
 
+		double area,volumen;
 		
-		globalradio = Double.parseDouble(JOptionPane.showInputDialog("ingresa el radio"));
+		globalradio = Double.parseDouble(JOptionPane.showInputDialog("Ingresa el radio"));  
 		
+		area = CalcularArea(globalradio);
+		volumen = CalcularVolumen (globalvolumen);
+			
+		System.out.println("El area del circulo es :"+ area);   
 
-		globalradio = Math.PI * Math.pow(globalradio, 2);
-		globalvolumen = (4.0/3.0) * Math.PI  *(Math.pow(globalradio, 3));
-		
-
-		System.out.println("El area del circulo es :"+ globalradio);   
-
-		System.out.println("El volumen de la esfera es :"+ globalvolumen);   
-
-
+		System.out.println("El volumen de la esfera es :"+ volumen);  
 	}
+	
+	public static double CalcularArea (double globalradio) {
+		
+		double area =  Math.PI * Math.pow(globalradio, 2);
+		return area;
+				
+		
+	}
+
+	public static double CalcularVolumen (double globalvolumen) {
+		
+		double volumen =  (4.0/3.0) * Math.PI  *(Math.pow(globalradio, 3));
+		return volumen;
+	
+}
 
 }
